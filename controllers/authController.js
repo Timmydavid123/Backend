591477b5b6b36b0b07c035a80781e408f07c80b8
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2023-12-22 15:12:11 +0100

    mv

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,33 +21,33 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const existingUser = await User.findOne({ googleId: profile.id });
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         const existingUser = await User.findOne({ googleId: profile.id });
 
-        if (existingUser) {
-          return done(null, existingUser);
-        }
+//         if (existingUser) {
+//           return done(null, existingUser);
+//         }
 
-        const newUser = await User.create({
-          googleId: profile.id,
-          email: profile.emails[0].value,
-        });
+//         const newUser = await User.create({
+//           googleId: profile.id,
+//           email: profile.emails[0].value,
+//         });
 
-        return done(null, newUser);
-      } catch (error) {
-        return done(error, null);
-      }
-    }
-  )
-);
+//         return done(null, newUser);
+//       } catch (error) {
+//         return done(error, null);
+//       }
+//     }
+//   )
+// );
 
 const authController = {
   signup: async (req, res) => {
