99142791b04fb71f521f8e970b9be8b919a6fe22
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2023-12-22 15:36:17 +0100

    kjb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+// Import the necessary modules
 require('dotenv').config();
+const crypto = require('crypto');
 const helmet = require('helmet');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,6 +9,10 @@ const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
 const extractUserId = require('./middleware/extractUserId')
+
+// Generate a secure random string as the session secret key
+const randomBuffer = crypto.randomBytes(32);
+const sessionSecretKey = randomBuffer.toString('hex');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -24,18 +30,15 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false}))
-
+app.use(express.urlencoded({ extended: false }));
 
 // Using helmet middleware for secure headers
 app.use(helmet());
 
-// Initialize Passport and use the express-session middleware
-app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+// Initialize Passport and use the express-session middleware with the generated secret key
+app.use(session({ secret: sessionSecretKey, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 // Routes
 app.use('/api', authRoutes);
