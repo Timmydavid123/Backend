a78dd0fed84fe84a7ff3467dcfbbdcd0a87e8fb0
Timmydavid123 <114180127+Timmydavid123@users.noreply.github.com>
2024-01-16 13:34:54 +0100

    hb

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const passport = require('passport');
 const session = require('express-session');
 const extractUserId = require('./middleware/extractUserId');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth'); 
+const authRoutes = require('./routes/auth');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const adminRoutes = require('./routes/admin');
+
 // Generate a secure random string as the session secret key
 const randomBuffer = crypto.randomBytes(32);
 const sessionSecretKey = randomBuffer.toString('hex');
@@ -29,18 +30,18 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
     console.error('MongoDB connection error:', error);
   });
 
-  // Serve the React app
+// Serve the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle 404 errors
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'build', '404.html'));
 });
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-
 
 // Define corsOptions before using it
 const corsOptions = {
@@ -51,9 +52,6 @@ const corsOptions = {
   allowedHeaders: 'Content-Type, Authorization',
 };
 
-// Handle preflight requests
-app.options('http://localhost:3000', cors(corsOptions));
-
 // Use the CORS middleware for all routes
 app.use(cors(corsOptions));
 
@@ -63,27 +61,10 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// Using helmet middleware for secure headers
-app.use(helmet());
-
-
-// Initialize Passport and use the express-session middleware with the generated secret key
-app.use(session({ secret: sessionSecretKey, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// app.use((err, req, res, next) => {
-//   if (req.method === 'OPTIONS') {
-//     res.status(204).end();
-//   } else {
-//     console.error(err.stack);
-//     res.status(500).send('Something went wrong!');
-//   }
-// });
 // Using helmet middleware for secure headers
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy(/* your CSP configuration */));
+
 // Initialize Passport and use the express-session middleware with the generated secret key
 app.use(session({ secret: sessionSecretKey, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -92,6 +73,7 @@ app.use(passport.session());
 // Routes
 app.use('/api', authRoutes);
 app.use('/admin', adminRoutes);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
